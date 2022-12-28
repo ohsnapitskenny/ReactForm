d87d598e7ad65d27857a369a762de00a29183fe3
Kenny Lam <hk_lam@live.com>
2022-12-28 13:48:22 +0100

    implemented async with hacky solution

--- src/RegistrationForm/RegistrationForm.js
+++ src/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,5 @@
 import './RegistrationForm.css';
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import FormInput from '../components/FormInput/formInput';
 
 const RegistrationForm = () => {
@@ -10,18 +10,21 @@ const RegistrationForm = () => {
     password: "",
     confirmPassword: "",
   });
+  const [formValid, setFormStatus] = useState(false);
+
+  const [users, setUser] = useState([]);
 
   const inputFields = [
     {
-      id: 1,
+      _id: 1,
       name: "firstName",
       type: "text",
-      placeholder: "Firstname",
+      placeholder: "FirstName",
       label: "Firstname",
       required: true,
     },
     {
-      id: 2,
+      _id: 2,
       name: "lastName",
       type: "text",
       placeholder: "Lastname",
@@ -29,7 +32,7 @@ const RegistrationForm = () => {
       required: true,
     },
     {
-      id: 3,
+      _id: 3,
       name: "email",
       type: "email",
       placeholder: "Email",
@@ -38,7 +41,7 @@ const RegistrationForm = () => {
       required: true,
     },
     {
-      id: 4,
+      _id: 4,
       name: "password",
       type: "password",
       placeholder: "Password",
@@ -49,7 +52,7 @@ const RegistrationForm = () => {
       required: true,
     },
     {
-      id: 5,
+      _id: 5,
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
@@ -62,33 +65,81 @@ const RegistrationForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-      validateIfPasswordContainsNames()
 
-    // Post Async and get
+    if (!formValid) {
+      // Button should be disabled when form is invalid.
+      alert("There is a name in the password")
+      return
+    }
+
+    createUser(values)
+
+    getUsers().then((res) => {
+      setUser(res)
+    })
+      .catch((e) => {
+        alert(e.message)
+      })
   };
 
+  //#region GET / POST Methods
+  //Concept
+  // const httpClient = async (url, method, headers, body) => {
+  //   const response = await fetch(`https://demo-api.vercel.app/users`,);
+  //   return await response.json();
+  // }
+
+  const getUsers = async () => {
+    const response = await fetch(`https://demo-api.vercel.app/users`);
+    const result = await response.json()
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    alert("User has been fetched!")
+    return result;
+  }
+
+  const createUser = (data) => {
+    const user = { firstName: data.firstName, lastName: data.lastName, email: data.email };
+    postUser(user).then((res) => {
+    alert("User has been created!");
+    })
+  };
+
+  const postUser = async (user) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user)
+    };
+    return await fetch(`https://demo-api.vercel.app/users`, requestOptions);
+  }
+  //#endregion
+
+  //#region Form Methods
   const handleInputChange = (e) => {
     const formFieldName = e.target.name;
     if (formFieldName === "password") {
-      validateIfPasswordContainsNames()
+      setFormStatus(isValidPasswordWithoutNames())
     }
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  function validateIfPasswordContainsNames() { // Place Validation in FormInput
-    if (values.password.search(values.firstName) === 0 || values.password.search(values.lastName) === 0){
-      alert("There is a name found in the password")
+  function isValidPasswordWithoutNames() {
+    if (values.password.search(values.firstName) === 0 || values.password.search(values.lastName) === 0) {
+      return false
     }
+    return true
   }
+  //#endregion
 
   return (
     <div className='container'>
       <form onSubmit={handleSubmit} >
-      <h2>Sign Up Form</h2>
+        <h2>Sign Up Form</h2>
+        <h2>Form Value: {formValid}</h2>
         <div className='form-body'>
           {inputFields.map((input) => (
             <FormInput
-              key={input.id}
+              key={input._id}
               {...input}
               value={values[input.name]}
               onChange={handleInputChange}
@@ -96,7 +147,7 @@ const RegistrationForm = () => {
           ))}
         </div>
         <div className='form-footer'>
-          <button type="submit">Sign Up</button>
+          <button type="submit" disabled={false} >Sign Up</button>
         </div>
       </form>
     </div>
